1da0103e6e62d2854fe960fd2fb288758f84264c
MargaridaTeles <telesmaegarida@gmail.com>
2023-03-23 15:33:06 +0000

    PowerOf2Until5

---
+++ Power2Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Power2Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        private static void PowerOf2Until5()
+        {
+            for(int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
