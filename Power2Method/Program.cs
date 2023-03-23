0d4cd72b851b1372a97073ff5f88f92a08b1f035
MargaridaTeles <telesmaegarida@gmail.com>
2023-03-23 20:37:38 +0000

    PowerOf2UntilN

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,6 +4,10 @@ namespace Power2Method
 {
     class Program
     {
+        /// <summary>
+        /// Mostrar até à 5ª potência de 2
+        /// </summary>
+        /// <param name="args"> Argumentos recebidos na consola</param>
         static void Main(string[] args)
         {
             PowersOf2Until5();
@@ -11,10 +15,17 @@ namespace Power2Method
             PowerOf2UntilN(7);
             PowerOf2UntilN(10);
         }
+        /// <summary>
+        ///  Mostrar até à 5ª potência de 2
+        /// </summary>
         private static void PowersOf2Until5()
         {
             PowerOf2UntilN(5);
         }
+        /// <summary>
+        ///  Mostra até à N potência de 2
+        /// </summary>
+        /// <param name="n"> Número inteiro que define o limite da potência de 2</param>
         private static void PowerOf2UntilN(int n)
         {
             for(int i = 1; i <= (1 << n); i <<= 1)
