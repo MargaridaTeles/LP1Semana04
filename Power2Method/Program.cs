28cc0532d6c902e2482626e9180c5979f431f7e0
MargaridaTeles <telesmaegarida@gmail.com>
2023-03-23 20:45:26 +0000

    PowerOf2UntilN N1 N2

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -14,6 +14,8 @@ namespace Power2Method
 
             PowerOf2UntilN(7);
             PowerOf2UntilN(10);
+
+            PowerOf2UntilN(7, 10);
         }
         /// <summary>
         ///  Mostrar até à 5ª potência de 2
@@ -29,6 +31,18 @@ namespace Power2Method
         private static void PowerOf2UntilN(int n)
         {
             for(int i = 1; i <= (1 << n); i <<= 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        /// <summary>
+        /// Mostra a potência de 2 entre n1 e n2.
+        /// </summary>
+        /// <param name="n1"> Primeiro valor potência de 2</param>
+        /// <param name="n2"> Ultímo valor da potência de 2</param>
+        private static void PowerOf2UntilN(int n1, int n2)
+        {
+            for (int i = 1 << n1; i <= (1 << n2); i <<= 1)
             {
                 Console.WriteLine(i);
             }
