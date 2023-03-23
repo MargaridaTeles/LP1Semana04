f64cc4f9b76c72d46e6c639bcfd698bcbf60ab36
MargaridaTeles <telesmaegarida@gmail.com>
2023-03-23 21:22:43 +0000

    Método adicionado

--- Lucas/Program.cs
+++ Lucas/Program.cs
@@ -6,7 +6,23 @@ namespace Lucas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Lucas(7));
+        }
+
+        private static int Lucas(int n)
+        {
+            if(n == 0)
+            {
+                return 2;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return (Lucas(n - 2) + Lucas(n - 1));
+            }
         }
     }
 }
