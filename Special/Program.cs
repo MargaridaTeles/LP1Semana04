4fb635d40f1cd907a68e78e34ef13c2dcc3670e8
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-11 13:23:37 +0000

    Add new method GetSpecial and update method main

---
+++ Special/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Special
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetSpecial(num));
+        }
+
+        private static int GetSpecial(int n)
+        {
+            int special;
+
+            if (n == 0)
+                special = 0;
+            else if (n == 1)
+                special = 1;
+            else
+                special = GetSpecial(n - 1) + 2 * GetSpecial(n -2);
+
+            return special;
+        }
+    }
+}
