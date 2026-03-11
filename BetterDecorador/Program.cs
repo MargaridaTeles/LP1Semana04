88b28c2e0ea8c54afc01e8ae027aa15f22a6ffbe
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-11 12:47:54 +0000

    Update method main to more efficience

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sentence = args[0];
+            char c = char.Parse(args[1]);
+            int num = int.Parse(args[2]);
+            
+            Console.WriteLine(Decor(sentence, c, num));
+        }
+
+        private static string Decor(string s, char dec, int num)
+        {
+            return $"{new string(dec, num)} {s} {new string(dec, num)}";
+        }
+    }
+}
