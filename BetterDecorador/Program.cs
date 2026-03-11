80ddcc0410c7289a52beb3fb7f3d18644cb70080
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-11 12:51:58 +0000

    Add XML comments

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -4,6 +4,10 @@ namespace BetterDecorador
 {
     public class Program
     {
+        /// <summary>
+        /// Convert the inputs and show the method Decor
+        /// </summary>
+        /// <param name="args">Input from user</param>
         private static void Main(string[] args)
         {
             string sentence = args[0];
@@ -13,6 +17,13 @@ namespace BetterDecorador
             Console.WriteLine(Decor(sentence, c, num));
         }
 
+        /// <summary>
+        /// Method have responsibility to make the text with the right specifications
+        /// </summary>
+        /// <param name="s">the string</param>
+        /// <param name="dec">the character</param>
+        /// <param name="num">the number of repetitions</param>
+        /// <returns>The text with the decoration</returns>
         private static string Decor(string s, char dec, int num)
         {
             return $"{new string(dec, num)} {s} {new string(dec, num)}";
